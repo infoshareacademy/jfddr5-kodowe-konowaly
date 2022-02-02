72cb21f89bba13e15d25e5bbcb51d7db824e9ac6
Damian Sulej <s4marx@gmail.com>
2022-02-02 12:02:44 +0000

    KOD-18 adds Routing

--- kwik/src/App.js
+++ kwik/src/App.js
@@ -1,7 +1,29 @@
+import React from "react";
+import { BrowserRouter, Route, Routes, NavLink } from "react-router-dom";
+import MainPage from "./subpages/MainPage";
+import Addmeme from "./subpages/Addmeme";
+import Top from "./subpages/Top";
+import Waitingroom from "./subpages/Waitingroom";
+import { LoginRegister } from "./subpages/LoginRegister";
 
 function App() {
   return (
-   <div></div> 
+    <BrowserRouter>
+      <NavLink to="/">MainPage</NavLink>
+      <NavLink to="/Topka">Topka</NavLink>
+      <NavLink to="/Login/register">
+        <button>Login</button>
+      </NavLink>
+      <NavLink to="/Poczekalnia">Poczekalnia</NavLink>
+      <NavLink to="/AdKwik">Dodaj Kwika</NavLink>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/AddKwik" element={<Addmeme />} />
+        <Route path="/Topka" element={<Top />} />
+        <Route path="/Poczekalnia" element={<Waitingroom />} />
+        <Route path="/Login/register" element={<LoginRegister />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ kwik/src/subpages/Addmeme.js
@@ -0,0 +1,12 @@
+function Addmeme() {
+  return (
+    <div>
+      <input type="file">Wybierz Plik</input>
+      <input placeholder="Nazwa"></input>
+      <input placeholder="#tagi"></input>
+      <button>Publikuj</button>
+    </div>
+  );
+}
+
+export default Addmeme;
