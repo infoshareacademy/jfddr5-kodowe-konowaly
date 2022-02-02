285cb35156eae0bd149f533dac1c5e8dee1e540d
Michal Wierzbicki <michalwierzbickipl@gmail.com>
2022-02-02 11:17:06 +0100

    add header files

---
+++ kwik/src/Header/nav.js
@@ -0,0 +1,18 @@
+function Nav() {
+  return (
+    <>
+      <div>
+        <div>
+          <img alt="logo" src=""></img>
+        </div>
+        <button class="addkwik">Dodaj kwika</button>
+        <button>Topka</button>
+        <button>Poczekalnia</button>
+        <button>Logowanie</button>
+        <button>Rejestracja</button>
+      </div>
+    </>
+  );
+}
+
+export default
