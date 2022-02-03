8a112135e2767044c8694d81a7c5331af42850d4
Damian Sulej <s4marx@gmail.com>
2022-02-03 15:06:41 +0000

    KOD-3-work-in-progress

--- kwik/src/App.js
+++ kwik/src/App.js
@@ -1,4 +1,3 @@
-
 import Nav from "./Header/nav";
 import React from "react";
 import { BrowserRouter, Route, Routes, NavLink } from "react-router-dom";
@@ -7,22 +6,21 @@ import Addmeme from "./subpages/Addmeme";
 import Top from "./subpages/Top";
 import Waitingroom from "./subpages/Waitingroom";
 import { LoginRegister } from "./subpages/LoginRegister";
-
 import { collection, getDocs, doc } from "firebase/firestore";
 import { useState, useEffect } from "react";
 import db from "./db";
+import up from "./img/up.png";
+import down from "./img/down.png";
 
 function App() {
   const [Kwik, setKwik] = useState([]);
-  
-
 
   const getKwik = async () => {
     const KwikCollection = collection(db, "Kwik");
     const KwikDocuments = await getDocs(KwikCollection);
 
     const KwikList = KwikDocuments.docs.map((doc) => ({
-      id: doc.id ,
+      id: doc.id,
       data: doc.data(),
     }));
 
@@ -33,11 +31,43 @@ function App() {
     getKwik();
   }, []);
 
+  const incrementVotes = (id) => {
+    setKwik((prevValue) => {
+      return prevValue.map((kwik) => {
+        return kwik.id !== id
+          ? kwik
+          : { ...kwik, data: { ...kwik.data, Votes: kwik.data.Votes + 1 } };
+      });
+    });
+  };
+
+  const DincrementVotes = (id) => {
+    setKwik((prevValue) => {
+      return prevValue.map((kwik) => {
+        return kwik.id !== id
+          ? kwik
+          : { ...kwik, data: { ...kwik.data, Votes: kwik.data.Votes - 1 } };
+      });
+    });
+  };
+
   const renderKwik = () =>
     Kwik.map((KwikElement) => (
       <div key={KwikElement.id}>
         <div>{KwikElement.data.Title}</div>
         <img style={{ width: "400px" }} src={KwikElement.data.URL} />
+        <p>{KwikElement.data.Votes}</p>
+
+        <img
+          style={{ width: "30px" }}
+          src={up}
+          onClick={() => incrementVotes(KwikElement.id)}
+        ></img>
+        <img
+          style={{ width: "30px" }}
+          src={down}
+          onClick={() => DincrementVotes(KwikElement.id)}
+        ></img>
         <hr />
       </div>
     ));
@@ -45,7 +75,7 @@ function App() {
 
   return (
     <BrowserRouter>
-    <Nav />
+      <Nav />
       <NavLink to="/">MainPage</NavLink>
       <NavLink to="/Topka">Topka</NavLink>
       <NavLink to="/Login/register">Login</NavLink>
@@ -61,7 +91,6 @@ function App() {
       <div> {renderKwik()}</div>
     </BrowserRouter>
   );
-
 }
 
 export default App;
--- kwik/src/subpages/Addmeme.js
+++ kwik/src/subpages/Addmeme.js
@@ -1,7 +1,12 @@
 import { useState } from "react";
 import { setDoc, doc } from "firebase/firestore";
 import db from "../db";
-import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
 
 function Addmeme({ fetchKwik }) {
   const [Id, setId] = useState("");
@@ -16,25 +21,27 @@ function Addmeme({ fetchKwik }) {
     // Create the file metadata
     /** @type {any} */
     const metadata = {
-      contentType: 'image/jpeg'
+      contentType: "image/jpeg",
     };
 
     // Upload file and metadata to the object 'images/mountains.jpg'
-    const storageRef = ref(storage, 'images/' + file.name);
+    const storageRef = ref(storage, "images/" + file.name);
     const uploadTask = uploadBytesResumable(storageRef, file, metadata);
 
     // Listen for state changes, errors, and completion of the upload.
-    uploadTask.on('state_changed',
+    uploadTask.on(
+      "state_changed",
       (snapshot) => {
         // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        console.log('Upload is ' + progress + '% done');
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log("Upload is " + progress + "% done");
         switch (snapshot.state) {
-          case 'paused':
-            console.log('Upload is paused');
+          case "paused":
+            console.log("Upload is paused");
             break;
-          case 'running':
-            console.log('Upload is running');
+          case "running":
+            console.log("Upload is running");
             break;
         }
       },
@@ -42,35 +49,33 @@ function Addmeme({ fetchKwik }) {
         // A full list of error codes is available at
         // https://firebase.google.com/docs/storage/web/handle-errors
         switch (error.code) {
-          case 'storage/unauthorized':
+          case "storage/unauthorized":
             // User doesn't have permission to access the object
             break;
-          case 'storage/canceled':
+          case "storage/canceled":
             // User canceled the upload
             break;
 
           // ...
 
-          case 'storage/unknown':
+          case "storage/unknown":
             // Unknown error occurred, inspect error.serverResponse
             break;
         }
       },
       () => {
         // Upload completed successfully, now we can get the download URL
-        getDownloadURL(uploadTask.snapshot.ref).then(async(downloadURL) => {
-          console.log('File available at', downloadURL);
-          await setDoc(doc(db, "Kwik", Id), {
+        getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+          console.log("File available at", downloadURL);
+          await setDoc(doc(db, "Kwik", Id) {
             Title,
-            URL:downloadURL,
+            URL: downloadURL,
             NameTag,
+            Votes: 0,
           });
         });
       }
     );
-
-
-   
   };
 
   const handleSubmit = (e) => {
@@ -87,9 +92,24 @@ function Addmeme({ fetchKwik }) {
     <div>
       <form onSubmit={handleSubmit}>
         <input type="file" onChange={(e) => setKwik(e.target.files[0])} />
-        <input type="text" placeholder="NazwaKwika" value={Id} onChange={(e) => setId(e.target.value)} />
-        <input type="text" placeholder="Tytuł" value={Title} onChange={(e) => setTitle(e.target.value)} />
-        <input type="text" placeholder="#tagi" value={NameTag} onChange={(e) => setNameTag(e.target.value)} />
+        <input
+          type="text"
+          placeholder="NazwaKwika"
+          value={Id}
+          onChange={(e) => setId(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Tytuł"
+          value={Title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="#tagi"
+          value={NameTag}
+          onChange={(e) => setNameTag(e.target.value)}
+        />
         <button type="submit">Publikuj</button>
       </form>
     </div>
