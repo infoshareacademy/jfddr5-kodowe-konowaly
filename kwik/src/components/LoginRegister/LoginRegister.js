6310536f9efba67488822714e1d6d203509b3f3b
Małgorzata Szymczak <gosia@szymczak.at>
2022-02-09 00:30:40 +0100

    KOD-41 validation done

--- kwik/src/components/LoginRegister/LoginRegister.js
+++ kwik/src/components/LoginRegister/LoginRegister.js
@@ -3,69 +3,142 @@ import { useForm } from "react-hook-form";
 
 const LoginRegister = () => {
   const {
-    register,
-    handleSubmit,
-    formState: { errors },
+    register: registerLogin,
+    handleSubmit: handleLoginSubmit,
+    formState: { errors: loginErrors },
   } = useForm();
+
+  const {
+    register: registerNewUser,
+    handleSubmit: handleNewUserSubmit,
+    formState: { errors: newUserErrors },
+  } = useForm();
+
   const onSubmit = (values) => console.log(values);
   return (
     <div className={s.form}>
       <div className={s.loginForm}>
         <h1 className={s.headings}>Logowanie</h1>
 
-        <form className={s.formForLoginInput} onSubmit={handleSubmit(onSubmit)} >
+        <form className={s.formForLogin} onSubmit={handleLoginSubmit(onSubmit)}>
           <input
             className={s.basicInput}
             name="name"
             type="text"
             placeholder="Nazwa Użytkownika"
-            {...register("name", {
-              required: true,
+            aria-label="Nazwa Użytkownika"
+            {...registerLogin("name", {
+              required: { value: true, message: "Wpisz nazwę użytkownika" },
               maxLength: {
                 value: 20,
+                message:
+                  "Nazwa użytkownika może posiadać maksymalnie 20 znaków",
               },
             })}
           />{" "}
-          {errors.name && (
-            <p>Nazwa użytkownika nie może mieć więcej niż 20 znaków.</p>
+          {loginErrors.name && (
+            <p className={s.error}>{loginErrors.name.message}</p>
           )}
           <input
             name="password"
             className={s.basicInput}
             type="password"
             placeholder="Hasło"
-            {...register("password", { 
-              required: true,
+            aria-label="Hasło"
+            {...registerLogin("password", {
+              required: { value: true, message: "Wpisz hasło" },
               minLength: {
                 value: 8,
-              }
+                message: "Hasło musi posiadać co najmniej 8 znaków.",
+              },
             })}
           />{" "}
-          {errors.password && (
-            <p>Hasło musi posiadać co najmniej 8 znaków.</p>
+          {loginErrors.password && (
+            <p className={s.error}>{loginErrors.password.message}</p>
           )}
           <input type="submit" value="Zaloguj" className={s.loginButton} />
         </form>
         <br></br>
-        <a href="">Nie pamietam hasła</a>
+        <a href="">Nie pamiętam hasła</a>
       </div>
 
       <div className={s.registerForm}>
         <h1 className={s.headings}>Rejestracja</h1>
-        <input
-          className={s.basicInput}
-          type="username"
-          placeholder="Nazwa Użytkownika"
-        />
-        <input className={s.basicInput} type="email" placeholder="Email" />
-        <input className={s.basicInput} type="password" placeholder="Hasło" />
-        <div className={s.checkbox}>
-          <input type="checkbox" id="check" name="check" />
-          <label for="check">
-            Przeczytałem i akceptuję regulamin oraz politykę prywatności
-          </label>
-        </div>
-        <button className={s.registerButton}>Zarejestruj się </button>
+        <form onSubmit={handleNewUserSubmit(onSubmit)}>
+          <input
+            className={s.basicInput}
+            type="username"
+            name="name"
+            placeholder="Nazwa Użytkownika"
+            aria-label="Nazwa Użytkownika"
+            {...registerNewUser("name", {
+              required: { value: true, message: "Wpisz nazwę użytkownika" },
+              maxLength: {
+                value: 20,
+                message:
+                  "Nazwa użytkownika może posiadać maksymalnie 20 znaków",
+              },
+            })}
+          />
+          {newUserErrors.name && (
+            <p className={s.error}>{newUserErrors.name.message}</p>
+          )}
+          <input
+            className={s.basicInput}
+            type="email"
+            name="email"
+            placeholder="Email"
+            aria-label="Email"
+            {...registerNewUser("email", {
+              required: { value: true, message: "Wpisz poprawny adres email" },
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: "Nieprawidłowy adres email.",
+              },
+            })}
+          />{" "}
+          {newUserErrors.email && (
+            <p className={s.error}>{newUserErrors.email.message}</p>
+          )}
+          <input
+            className={s.basicInput}
+            type="password"
+            placeholder="Hasło"
+            aria-label="Hasło"
+            {...registerNewUser("password", {
+              required: { value: true, message: "Wpisz hasło" },
+              minLength: {
+                value: 8,
+                message: "Hasło musi posiadać co najmniej 8 znaków.",
+              },
+            })}
+          />
+          {newUserErrors.password && (
+            <p className={s.error}>{newUserErrors.password.message}</p>
+          )}
+          <div className={s.checkbox}>
+            <input
+              type="checkbox"
+              id="check"
+              name="check"
+              {...registerNewUser("checkbox", {
+                required: { value: true, message: "Akceptacja jest wymagana" },
+              })}
+            />
+            <label type="checkbox">
+              Przeczytałem i akceptuję regulamin oraz politykę prywatności
+            </label>
+           
+          </div>
+          {newUserErrors.checkbox && (
+              <p className={s.error}>{newUserErrors.checkbox.message}</p>
+            )}
+          <input
+            type="submit"
+            value="Zarejestruj się"
+            className={s.registerButton}
+          />
+        </form>
       </div>
     </div>
   );
