75f9f75c1d340577c6a95a66b110edbc076b2d6d
Małgorzata Szymczak <gosia@szymczak.at>
2022-02-08 20:51:34 +0100

    KOD041 validation for login done

--- kwik/src/components/LoginRegister/LoginRegister.js
+++ kwik/src/components/LoginRegister/LoginRegister.js
@@ -1,22 +1,55 @@
 import s from "./LoginRegister.module.css";
+import { useForm } from "react-hook-form";
 
 const LoginRegister = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (values) => console.log(values);
   return (
     <div className={s.form}>
       <div className={s.loginForm}>
         <h1 className={s.headings}>Logowanie</h1>
-        <input
-          className={s.basicInput}
-          type="text"
-          placeholder="Nazwa Użytkownika"
-        />
-        <input className={s.basicInput} type="password" placeholder="Hasło" />
-        <button type="login" className={s.loginButton}>
-          Zaloguj
-        </button>
+
+        <form className={s.formForLoginInput} onSubmit={handleSubmit(onSubmit)} >
+          <input
+            className={s.basicInput}
+            name="name"
+            type="text"
+            placeholder="Nazwa Użytkownika"
+            {...register("name", {
+              required: true,
+              maxLength: {
+                value: 20,
+              },
+            })}
+          />{" "}
+          {errors.name && (
+            <p>Nazwa użytkownika nie może mieć więcej niż 20 znaków.</p>
+          )}
+          <input
+            name="password"
+            className={s.basicInput}
+            type="password"
+            placeholder="Hasło"
+            {...register("password", { 
+              required: true,
+              minLength: {
+                value: 8,
+              }
+            })}
+          />{" "}
+          {errors.password && (
+            <p>Hasło musi posiadać co najmniej 8 znaków.</p>
+          )}
+          <input type="submit" value="Zaloguj" className={s.loginButton} />
+        </form>
         <br></br>
         <a href="">Nie pamietam hasła</a>
       </div>
+
       <div className={s.registerForm}>
         <h1 className={s.headings}>Rejestracja</h1>
         <input
